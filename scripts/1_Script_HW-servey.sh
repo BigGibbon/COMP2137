ff851c7aadd56189557ad7122bf597e298d37efb
Austin Connor <Austin.Connor@mygeorgian.ca>
2026-06-10 02:07:31 -0400

    FINAL. All comments have been redone, and script is show to be functional on my computer

--- scripts/1_Script_HW-servey.sh
+++ scripts/1_Script_HW-servey.sh
@@ -1,20 +1,19 @@
 #!/bin/bas
 
-# Place the current date in the formate of YYYY-MM-DD into a variable called DATE_NOW via the data command to be printed in the report
+# Place the current date in the format of YYYY-MM-DD into a variable called DATE_NOW via the data command to be printed in the report
 DATE_NOW=$(date +"%Y-%m-%d")
 
-# This gets the distrobution name and places it into a variable called DISTRO_NAME to be printed in the report
+# This gets the distro name by running the /etc/os-release as source thus giving us access to the variables from it, including $NAME
 DISTRO_NAME=$(source /etc/os-release; echo $NAME)
 
-# This gets the distrobution version and places it into a variable called DISTRO_VERSION to be printed in the report
+# SAME as distro name but with $VERSION
 DISTRO_VERSION=$(source /etc/os-release; echo $VERSION)
 
 # This gets the uptime and places it into a variable called UPTIME to be printed in the report
 UPTIME=$(uptime)
 
-# This gets the CPU make and model and puts it into a variable called CPU_INFO to be printed in the report
-# it does this by first listing the CPU section of lshw, then it uses grep to find only the product lines (of which there are multiple),
-# then it uses awk to grab only the first line of the product lines (i have multiple) from grep (all of which are duplicates),
+# First we list the CPU section of lshw, then it uses grep to find only the product line(s),
+# then it uses awk to grab only the first line of the product lines (in case there are multiple) from grep,
 # in that same awk replaces the first word (which is the "product:" that grep found) with a blank (""),
 # and lastly does a print so that the output can be placed into the MODEL variable. Same with the MAKE variable, but we use vendor.
 # then that gets echo'd out to the CPU_INFO var
@@ -22,25 +21,23 @@ CPU_INFO=$(MODEL=$(lshw -C cpu | grep product | awk 'NR==1 { $1=""; print }')
 	   MAKE=$(lshw -C cpu | grep vendor | awk 'NR==1 { $1=""; print }')
 	   echo $MAKE $MODEL)
 
-# THIS gets the RAM size and places it into a variable called RAM_SIZE to be printed in the report later
-# the process of getting the ram size is nearly identical to the CPU info exept we look for the memory section of lshw and we grep for size
-RAM_SIZE=$(lshw -C memory | grep size | awk 'NR==1 { $1=""; print }')
+# The process of getting the ram size is nearly identical to the CPU exept we look for the memory section of lshw, grep for size, and dont worry about the multilines
+RAM_SIZE=$(lshw -C memory | grep size | awk '{ $1=""; print }')
 
-#This gets the disks name, model, and size and places it intoa variable called DISKS to be printed in the report
 # This get the only the entries from lsblk of the type disk, displays only the name, model, and size
 # it then uses awk to replace all the \x20 (escape code for space which gets put in the output for some reason) with a space (" ")
 DISKS=$(lsblk --raw -o NAME,MODEL,SIZE -nQ 'TYPE == "disk"' | awk '{gsub(/\\x20/, " "); print}')
 
-# This gets the video card make and model and places it into the VIDEO variable to be printed in the report
-# refer to the CPU_INFO comments for a break down of what is happening as it is the same, just in the video section not the cpu section
-VIDEO=$(MODEL=$(lshw -C video | grep product | awk 'NR==1 { $1=""; print }') 
-        MAKE=$(lshw -C video | grep vendor | awk 'NR==1 { $1=""; print }') 
+# This is nearly an identical process to the CPU, but with the video part of lshw instead of the cpu part
+VIDEO=$(MODEL=$(lshw -C video | grep product | awk 'NR==1 { $1=""; print }')
+        MAKE=$(lshw -C video | grep vendor | awk 'NR==1 { $1=""; print }')
         echo $MAKE $MODEL)
 
-# This gets the host ip address connected to the default gateway and puts it into the HOST_ADDR varaible to be printed in the report
+# Uses ip route to get info about the route to the default gateway, greps for the word default as that is the line with the host address,
+# then uses awk to print out the host address
 HOST_ADDR=$(ip route | grep default | awk '{print $9}')
 
-# Uses ip route to get info about the route to the default gateway. greps for the word default to find the line where the default gatway IP is, 
+# Uses ip route to get info about the route to the default gateway, greps for the word default to find the line where the default gatway IP is,
 # then uses awk to print the default gateway IP
 DEFAULT_GATE=$(ip route | grep default | awk '{print $3}')
 
